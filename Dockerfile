d097a3bfec296f3a5d4b52e50af7527d2f02e014
qitas <qitas@qitas.cn>
2021-03-15 15:23:24 +0800

    add

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,5 @@ RUN apt update && apt install -y sdcc git make binutils
 COPY . /code
 WORKDIR /code
 RUN git submodule update --init
-WORKDIR /code/src
+WORKDIR /code/jtag
 RUN make
--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 set -eu
 
-PROJECT=ch55xjtag
+PROJECT=jtag
 
 docker build -t ${PROJECT} -f Dockerfile .
-docker run -v "${PWD}:/mnt" ${PROJECT} bash -c "cp -v /code/src/*.hex /mnt"
+docker run -v "${PWD}:/mnt" ${PROJECT} bash -c "cp -v /code/jtag/*.hex /mnt"
